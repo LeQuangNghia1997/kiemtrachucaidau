459539346b94a8451f8eaea3db27d5b7201fd750
lequangnghia <lequangnghia35@gmail.com>
2019-01-19 09:38:01 +0700

    bai tap va mang

---
+++ kiemtra.js
@@ -0,0 +1,9 @@
+regexp = /^[A-Z]/;
+function isFirstLetterUpperCase(str){
+    regexp = /^[A-Z]/;
+    if (regexp.test(str)) {
+        alert("String's first character is uppercase");
+    } else {
+        alert("String's first character is not uppercase");
+    }
+}
